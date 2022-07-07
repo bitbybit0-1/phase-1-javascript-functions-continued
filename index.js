260c3166ef2d973f7299c1ab0b5e541ac1723df1
Joseph Sutera <joesutera11@gmail.com>
2022-07-07 15:23:49 -0400

    commit tree 17e00d8387db5649de44b9039e8a7fd39c1db83b

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+// code your solution here)
+ function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+ }
+ function mondayWork(activity="go to the office"){
+return `This Monday, I will ${activity}.`
+ }
+ function wrapAdjective(x="*"){
+    return function(y="special"){
+        return `You are ${x}${y}${x}!`
+    }
+ }
